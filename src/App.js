a6fd3caaa8b1812f2457a54e8b5088b19e39fcd8
unknown <skubidu2@inbox.ru>
2022-03-17 12:17:21 +0400

    s

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import CV from 'react-cv'
-import { CVData } from './data';
+import CV from "react-cv";
+import { CVData } from "./data";
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
-      <div>
+      <div id="root">
         <CV {...CVData} />
       </div>
-    )
+    );
   }
 }
