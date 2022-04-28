2c520ddd7f032c66a280ba0988a91cf7e3eb100d
floydian910 <76868669+floydian910@users.noreply.github.com>
2022-04-28 23:35:58 +0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export const CVData = {
       "https://res.cloudinary.com/dzxxb95vy/image/upload/v1647422968/1597708634972_lcv76h.jpg",
     contacts: [
       { type: "email", value: "lgelava7@gmail.com" },
-       { type: "phone", value: "+995 592 02 78 50" },
+       { type: "phone", value: "+995 555 25 85 40" },
       { type: "phone", value: "+995 592 02 78 50" },
       { type: "location", value: "Tbilisi" },
     ],
